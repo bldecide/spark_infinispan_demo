902a43cbf34f22d971f52cee9effbfa8c3b06b6d
runner <jungungi@coupang.com>
2015-12-11 23:40:02 +0900

    sample 작성

--- src/main/scala/com/runner/demo/infinispan/RandomPut.scala
+++ src/main/scala/com/runner/demo/infinispan/RandomPut.scala
@@ -1,13 +1,13 @@
 package com.runner.demo.infinispan
 
-import org.infinispan.client.hotrod.{RemoteCache, RemoteCacheManager}
+import org.infinispan.client.hotrod.{RemoteCache, RemoteCacheManager, Search}
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder
 
 /**
  * Created by Runner on 2015. 12. 11..
  */
-object SimplePutGet {
-  def main(args : Array[String]): Unit = {
+object RandomPut {
+  def main(args: Array[String]): Unit = {
     // Create the remote cache manager
     val build = new ConfigurationBuilder().addServer().host("192.168.215.239").build()
     val remoteCacheManager = new RemoteCacheManager(build)
@@ -15,17 +15,17 @@ object SimplePutGet {
     // Obtain the remote cache
     val cache :RemoteCache[Integer, Integer] = remoteCacheManager.getCache("my-cache")
 
-    cache.put(-1, 10)
-    cache.put(-2, 10)
-    cache.put(-3, 10)
-    cache.put(-4, 10)
-    cache.put(-5, 10)
-    cache.put(-6, 10)
+    var count = 0
+    val r = scala.util.Random
+    while (true) {
+      val limit = r.nextInt(10)
+      for (i <- 1 to limit) {
+        count += 1
+        cache.put(count, count)
+      }
 
-
-    for (i <- -11 to 10) {
-      println(cache.get(i))
+      println(limit)
+      Thread sleep 1000
     }
-
   }
 }
